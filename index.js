97ac1860f3abf260158057977c868e92d8a6d182
Haa-meem <haameemalhamra@gmail.com>
2023-05-20 16:44:35 +0600

    server is running

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const port=process.env.PORT||5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+res.send('Toy Store is running')
+})
+
+app.listen(port,()=>{
+    console.log(`Toy Store server is running on port ${port}`)
+})
